02f48ec4ee02c02bfaffb0b33d0ba21939412fda
Otto <ottobohn187@gmail.com>
2026-08-13 18:57:04 -0700

    Bust mobile WebAssembly cache

--- web/mirror-pre.js
+++ web/mirror-pre.js
@@ -1,5 +1,6 @@
 var Module = {
   noInitialRun: false,
+  locateFile: function (path) { return path + "?build=fb2d002"; },
   print: function (text) { window.PimpTerminal.write(text + "\n"); },
   printErr: function (text) { window.PimpTerminal.write(text + "\n"); }
 };
